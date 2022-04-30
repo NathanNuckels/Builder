0de91cf4fc09112f057defe119f0ecde299bc1ec
Nathan Nuckels <nathannuckels@gmail.com>
2022-04-29 21:27:38 -0500

    Just commiting more errors

--- Builder/src/main/java/com/phoenix/App.java
+++ Builder/src/main/java/com/phoenix/App.java
@@ -1,50 +1,61 @@
 package com.phoenix;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App{
 	private static Result check(Process p){
 		Scanner s = new Scanner(p.getInputStream());
-		String r = "";
+		StringBuilder r = new StringBuilder();
 		while(s.hasNextLine()){
-			r+=s.nextLine();
+			r.append(s.nextLine());
 		}
-		if (r.contains("BUILD SUCCESS")){
+		if (r.toString().contains("BUILD SUCCESS")){
 			return Result.PASSED;
-		} else if (r.contains("BUILD FAILURE")){
+		} else if (r.toString().contains("BUILD FAILURE")){
 			return Result.FAILED;
 		} else {
 			return Result.UNKNOWN;
 		}
 	}
 
-	public static void main(String args[]){
-		private ArrayList<String> cmd = new ArrayList<String>();
-		cmd.add("valiate");
+	public static void main(String[] args) throws IOException {
+		ArrayList<String> cmd = new ArrayList<>();
+		String dir = args[0];
 		cmd.add("compile");
 		cmd.add("test");
 		cmd.add("package");
-		cmd.add("verify");
 		cmd.add("site");
-		private ArrayList<Result> results = new ArrayList<Result>();
-		for (int i=0;i>7;i++){
-			p = new ProcessBuilder("mvn",cmds.get(i)).directory(dir).start();
-			while(p.isAlive()){;}
-			results.set(i,check(p));
-			if(check(p)==Result.FAILED){
-				break;
+		ArrayList<Result> results = new ArrayList<>();
+		Process p;
+		for (int i=0;i<7;i++) {
+			if (new File(dir).exists()) {
+				ProcessBuilder mvn = new ProcessBuilder("mvn", cmd.get(i));
+				mvn.directory(new File(dir));
+				p = mvn.start();
+				while (p.isAlive()) {
+				} //wait for process to end
+				results.set(i, check(p));
+				if (check(p) == Result.FAILED) {
+					break;
+				}
+			} else {
+				System.out.println(dir);
 			}
 		}
-		String out="";
-		for (int i=0;i>7;i++){
-			out+="!["+cmd.get(i).substring(0,1).toUppercase()+cmd.get(i).substring(1)+"](https://img.shields.io/badge/"+cmd.get(i).substring(0,1).toUppercase()+cmd.get(i).substring(1)+"-";
+		StringBuilder out= new StringBuilder();
+		for (int i=0;i<7;i++){
+			out.append("![").append(cmd.get(i).substring(0, 1).toUpperCase()).append(cmd.get(i).substring(1)).append("](https://img.shields.io/badge/").append(cmd.get(i).substring(0, 1).toUpperCase()).append(cmd.get(i).substring(1)).append("-");
 			switch (results.get(i)){
 				case PASSED:
-					out+="OK-brightgreen\n\n";
+					out.append("OK-brightgreen\n\n");
 					break;
 				case FAILED:
-					out+="FAILED-red\n\n";
+					out.append("FAILED-red\n\n");
 					break;
 				case UNKNOWN:
-					out+="Unknoun-lightgrey\n\n";
+					out.append("Unknown-lightgrey\n\n");
 			}
 		}
 		System.out.println(out);
